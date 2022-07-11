40db3d582ec2a186cd8da0e5e00010c5ea20c552
SWELLZ <69326160+SWELLZ@users.noreply.github.com>
2022-07-10 18:43:22 -0700

    Add js functionality to dropdowns

---
+++ resources/interactivity.js
@@ -0,0 +1,12 @@
+function promoDropdown(){
+    document.getElementById('promo-content').style.display = 'block';
+}
+function wallpaperDropdown(){
+    document.getElementById('wallpaper-content').style.display = 'block';
+}
+function cmLuxuryDropdown(){
+    document.getElementById('cm-luxury-content').style.display = 'block';
+}
+function sfCoffeeDropdown(){
+    document.getElementById('sf-coffee-content').style.display = 'block';
+}
