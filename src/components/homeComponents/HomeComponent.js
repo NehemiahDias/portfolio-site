ab48d2c585f9cb70ee45eba50cb21b944fbe03c2
SWELLZ <microsoftdavid4@gmail.com>
2022-08-09 01:00:46 -0700

    change project component

--- src/components/homeComponents/HomeComponent.js
+++ src/components/homeComponents/HomeComponent.js
@@ -6,8 +6,8 @@ function HomeComonent() {
     <section id="welcome-section">
         <div className="welcome-bg">
             <p className="purple sub-text">Hi, my name is</p>
-            <h1 className="main-title">Nehemiah Dias</h1>
-            <h1 className="role-text">Front-End Web Developer</h1>
+            <h1 className="large-text">Nehemiah Dias</h1>
+            <h1 className="large-text off-purple">Front-End Web Developer</h1>
         </div>        
     </section>
   )
--- src/components/projectsComponents/ProjectComponent.js
+++ src/components/projectsComponents/ProjectComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ProjectComponent.css';
+import promoGen from '../images/promo-gen-ss.png';
 
 function ProjectComponent() {
   return (
@@ -7,24 +8,29 @@ function ProjectComponent() {
         <h1 className="section-title">Projects</h1>
         <div className="projects">
             <div className="project promo-code">
-
-                <h2 className="project-title">Promo Code Generator</h2>
-                <p>A command line project which was also my first ever project! The purpose was to generate a Promo Code and email it to a customers' email automatically. I made this for a business that I started years ago, it made me fall in love with programming because it showcased that something so simple can solve any problem, it also felt really good to be the one to solve that problem. Although not pretty, it was very functional and fast and pretty good for a beginner.</p>
-                <h3>What I used:</h3>
-                <ul>
-                    <li>Python</li>
-                    <li>GMAIL Libraries and APIs</li>
-                    <li>Text file parsing and read/writing</li>
-                </ul>
-                <h3>What I learned / Results</h3>
-                <ul>
-                    <li>Any problem can be solved with programming</li>
-                    <li>It made me want to keep learning</li>
-                    <li>Customers loved it!</li>
-                </ul>
-                <div className="project-buttons">
-                    <button><span className="button-icon">&lt;</span> Code <span className="button-icon">/&gt;</span></button>
-                    <button><i className="fa-solid fa-eye"></i> Live</button>
+                <div className='project-image'>
+                    <img src={promoGen} alt="Promo Gen Image"/>
+                </div>
+                
+                <div className='project-desc'>
+                    <h2 className="project-title off-purple">Promo Code Generator</h2>
+                    <p>A command line project which was also my first ever project! The purpose was to generate a Promo Code and email it to a customers' email automatically. I made this for a business that I started years ago, it made me fall in love with programming because it showcased that something so simple can solve any problem, it also felt really good to be the one to solve that problem. Although not pretty, it was very functional and fast and pretty good for a beginner.</p>
+                    <h3>What I used:</h3>
+                    <ul>
+                        <li>Python</li>
+                        <li>GMAIL Libraries and APIs</li>
+                        <li>Text file parsing and read/writing</li>
+                    </ul>
+                    <h3>What I learned / Results</h3>
+                    <ul>
+                        <li>Any problem can be solved with programming</li>
+                        <li>It made me want to keep learning</li>
+                        <li>Customers loved it!</li>
+                    </ul>
+                    <div className="project-buttons">
+                        <button><span className="button-icon">&lt;</span> Code <span className="button-icon">/&gt;</span></button>
+                        <button><i className="fa-solid fa-eye"></i> Live</button>
+                    </div>
                 </div>
             </div>
 
