0d0d0e79b817f9fae149f9e261542ebd55f32f30
SWELLZ <69326160+SWELLZ@users.noreply.github.com>
2022-08-10 00:09:41 -0700

    add contact styling and buttons

--- src/components/contactComponents/ContactComponent.js
+++ src/components/contactComponents/ContactComponent.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './ContactComponent.css';
 
 function ContactComponent() {
   return (
     <section id='contact-section'>
         <h1 className='section-title'>Contact Me</h1>
+        <div className='contact-buttons'>
+            <button><i class="fa-solid fa-envelope"></i> Email</button>
+            <button><i class="fa-brands fa-github"></i> Github</button>
+            <button><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+        </div>
     </section>
   )
 }
