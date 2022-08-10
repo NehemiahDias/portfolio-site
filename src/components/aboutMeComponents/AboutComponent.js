e1519341d2b38ca695ba9211963e0b5395059e57
SWELLZ <69326160+SWELLZ@users.noreply.github.com>
2022-08-09 23:33:28 -0700

    Update AboutComponent.js

--- src/components/aboutMeComponents/AboutComponent.js
+++ src/components/aboutMeComponents/AboutComponent.js
@@ -5,7 +5,7 @@ function AboutComponent() {
   return (
     <section id="about-me">
         <h1 className="section-title">About Me</h1>
-        <p>Hi, I am a self taught Front End Developer. So far, I have created projects to solve everyday problems that I have and I have been contributing to open source projects. Through my self taught journey, I have fallen in love with ReactJs, Redux, JavaScript, and Web Developing in general! I have been programming since I was around nine years old starting with Python and eventually tried Web Development; I instantly knew that it was the career path for me! Although self taught is not conventional route, I did take a 6-month course at CodeCademy and completed the Front-End Development path at freeCodeCamp. With that, I have been motivated and dedicated for over half my life to being a Web Developer and I wont stop there!</p>
+        <p>Hi, I am a self taught Front End Developer. So far, I have created projects to solve everyday problems that I have and I have been contributing to open source projects. Through my self taught journey, I have fallen in love with ReactJs, Redux, JavaScript, and Web Development in general! I have been programming since I was around nine years old starting with Python and eventually tried Web Development; I instantly knew that it was the career path for me! Although self taught is not conventional route, I did take a 6-month course at CodeCademy and completed the Front-End Development path at freeCodeCamp. With that, I have been motivated and dedicated for over half my life to being a Web Developer and I wont stop there!</p>
         <section id="skills">
           <h2 className='skills-title'>Skills</h2>
           <div className='skill-tab'>
