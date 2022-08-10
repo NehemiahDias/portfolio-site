010940ef0303f241ab3c5246b462756c72272721
SWELLZ <69326160+SWELLZ@users.noreply.github.com>
2022-08-09 18:58:20 -0700

    add about me

--- src/components/aboutMeComponents/AboutComponent.js
+++ src/components/aboutMeComponents/AboutComponent.js
@@ -4,6 +4,8 @@ function AboutComponent() {
   return (
     <section id="about-me">
         <h1 className="section-title">About Me</h1>
+        <p>Hi, my name is Nehemiah Dias and I am a Front End Developer. So far, I have only created projects to solve problems that I am having but I have been contributing to a bunch of open source projects. Through my self taught journey, I have fallen in love with ReactJs, Redux, JavaScript, and Web Developing in general! I have been programming since I was around nine years old starting with Python. I wasn't a big fan of data analytics or anything else Python was commonly used for so I ventured into C# for game development; It was much better but then I tried Web Development and I instantly knew that it was the career path for me! With that, I have been motivated and dedicated for over half my life to being a Web Developer and I wont stop there!</p>
+        <p>I went go to college for Computer Science at University of the Pacific for an entire semester! The reason I am not there anymore for the holy Bachelor's Degree is because I looked at their curriculum and realized I can learn way more on my own. So that's what I did! I dived into any and every resource I could get my hands on and learned more in a few months than I would have in four years at a University. Though it is not conventional route, I did take a 6-month course at CodeCademy and completed the Front-End Development path at freeCodeCamp. I also re-enrolled in College for an Associate's Degree at San Joaquin Delta College and I am continuing to learn and contribute to open source every day!</p>
     </section>
   )
 }
