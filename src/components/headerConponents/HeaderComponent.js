fa2c5e1bfef3d01076f9e5165d129583753a3839
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-08-11 06:11:07 -0700

    fix nav and burger animation

--- src/components/headerConponents/HeaderComponent.js
+++ src/components/headerConponents/HeaderComponent.js
@@ -5,7 +5,10 @@ import './HeaderComponent.css';
 
 function HeaderComponent() {
   const { theme, toggleTheme } = useContext(ThemeContext);
+
   var navRef = useRef();
+  var burgerRef = useRef();
+
   const headerStyle = {
     common: {
       transition: 'all 1s ease'
@@ -19,6 +22,13 @@ function HeaderComponent() {
   const toggleNav = () => {
     navRef.current.classList.toggle('nav-active');
   }
+  const toggleBurger = () => {
+    burgerRef.current.classList.toggle('toggle');
+  }
+  const actions = () => {
+    toggleNav();
+    toggleBurger();
+  }
 
   return (
     <header className={theme ==='dark' ? 'dark' : 'light'}>
@@ -28,10 +38,10 @@ function HeaderComponent() {
         </div>
         <nav id='navbar' style={themeStyle} ref={navRef}>
           <ul className="sub-text">
-              <li><a href='/#'><span className='purple'>{">"} </span>Home</a></li>
-              <li><a href='#projects-section'><span className='purple'>{">"} </span>Projects</a></li>
-              <li><a href='#about-me'><span className='purple'>{">"} </span>About</a></li>
-              <li><a href='#contact-section'><span className='purple'>{">"} </span>Contact</a></li>
+              <li><a onClick={actions} href='/#'><span className='purple'>{">"} </span>Home</a></li>
+              <li><a onClick={actions} href='#projects-section'><span className='purple'>{">"} </span>Projects</a></li>
+              <li><a onClick={actions} href='#about-me'><span className='purple'>{">"} </span>About</a></li>
+              <li><a onClick={actions} href='#contact-section'><span className='purple'>{">"} </span>Contact</a></li>
               <li><div className='switch-button'> {theme === 'dark' ? <i class="fa-solid fa-moon"></i> : <i className="fa-solid fa-sun"></i>}
               <Switch
               onChange={toggleTheme} 
@@ -43,10 +53,10 @@ function HeaderComponent() {
               /></div></li>
           </ul>
         </nav>
-         <div className='burger' onClick={toggleNav}>
-            <div className='line1'></div>
-            <div className='line2'></div>
-            <div className='line3'></div>
+         <div className='burger' ref={burgerRef} onClick={actions}>
+            <div className='line1' ></div>
+            <div className='line2' ></div>
+            <div className='line3' ></div>
           </div>
       </div>
     </header>
